e19f0a7e777c1914b509e41851711416e34c598d
Qlhs <josh@jibson.com>
2022-04-06 19:27:41 -0600

    make creation script and its dependencies

---
+++ Assets/Scripts/Creation.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+internal abstract class Creation : MonoBehaviour {
+    private int _health;
+    private int _damageOutput;
+    private int _range;
+    private DamageType _damageType;
+    private Player _owningPlayer;
+    private DamageType[] _resistances;
+    private DamageType[] _weaknesses;
+    private Interactable[] _interactables;
+    public abstract bool CanBeBuilt();
+}
